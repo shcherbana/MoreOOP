6f189396ab142d01a75d967de51e83fe3d36ccf6
shcherbana <shcherba.pn@ucu.edu.ua>
2023-10-07 20:17:43 +0300

    Modified strongkick

--- src/main/java/kick/StrongKick.java
+++ src/main/java/kick/StrongKick.java
@@ -6,6 +6,5 @@ public class StrongKick implements KickStrategy{
     @Override
     public void kick(Character whoKick, Character whoIsKicked) {
         whoIsKicked.setHp(whoIsKicked.getHp()-whoKick.getPower());
-        System.out.println("");
     }
 }
